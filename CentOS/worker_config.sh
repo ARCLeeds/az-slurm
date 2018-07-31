31bc14f043d0a34386562e1a2c0fb00ba89043fe
Mike Newton <jmnewton@duke.edu>
2018-07-31 12:02:34 -0400

    Remove slurm-contribs

--- CentOS/worker_config.sh
+++ CentOS/worker_config.sh
@@ -4,7 +4,7 @@ sudo chmod g-w /var/log
 sudo useradd -c "Slurm scheduler" slurm
 sudo yum -y install epel-release
 sudo yum -y install munge
-sudo yum -y install /tmp/slurm-17.11.8-1.el7.centos.x86_64.rpm /tmp/slurm-contribs-17.11.8-1.el7.centos.x86_64.rpm /tmp/slurm-example-configs-17.11.8-1.el7.centos.x86_64.rpm /tmp/slurm-libpmi-17.11.8-1.el7.centos.x86_64.rpm /tmp/slurm-pam_slurm-17.11.8-1.el7.centos.x86_64.rpm /tmp/slurm-slurmd-17.11.8-1.el7.centos.x86_64.rpm
+sudo yum -y install /tmp/slurm-17.11.8-1.el7.centos.x86_64.rpm  /tmp/slurm-example-configs-17.11.8-1.el7.centos.x86_64.rpm /tmp/slurm-libpmi-17.11.8-1.el7.centos.x86_64.rpm /tmp/slurm-pam_slurm-17.11.8-1.el7.centos.x86_64.rpm /tmp/slurm-slurmd-17.11.8-1.el7.centos.x86_64.rpm
 sudo cp -f /tmp/munge.key /etc/munge/munge.key
 sudo chown munge /etc/munge/munge.key
 sudo chgrp munge /etc/munge/munge.key
