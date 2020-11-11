ad62fcdfacaa54e24b2773531369861efa2c0152
John Hodrien <J.H.Hodrien@leeds.ac.uk>
2020-11-11 20:43:36 +0000

    Tidy up the redirect to log

--- CentOS/azuredeploy.sh
+++ CentOS/azuredeploy.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
+set -x
+exec >& /tmp/azuredeploy.log.$$
+
 # This script can be found on https://raw.githubusercontent.com/ARCLeeds/az-slurm/main/CentOS/azuredeploy.sh
 # This script is part of azure deploy ARM template
 # This script will install SLURM on a Linux cluster deployed on a set of Azure VMs
 
 # Basic info
-date > /tmp/azuredeploy.log.$$ 2>&1
-whoami >> /tmp/azuredeploy.log.$$ 2>&1
-echo $@ >> /tmp/azuredeploy.log.$$ 2>&1
+date
+whoami
+echo $@
 
 # Usage
 if [ "$#" -ne 9 ]; then
-  echo "Usage: $0 MASTER_NAME MASTER_IP WORKER_NAME WORKER_IP_BASE WORKER_IP_START NUM_OF_VM ADMIN_USERNAME ADMIN_PASSWORD TEMPLATE_BASE" >> /tmp/azuredeploy.log.$$
+  echo "Usage: $0 MASTER_NAME MASTER_IP WORKER_NAME WORKER_IP_BASE WORKER_IP_START NUM_OF_VM ADMIN_USERNAME ADMIN_PASSWORD TEMPLATE_BASE"
   exit 1
 fi
 
@@ -30,7 +33,7 @@ ADMIN_PASSWORD=$8
 TEMPLATE_BASE=$9
 
 # Update sudo rule for azureuser
-sudo sed -i -- 's/azureuser ALL=(ALL) ALL/azureuser ALL=(ALL) NOPASSWD:ALL/g' /etc/sudoers.d/waagent >> /tmp/azuredeploy.log.$$ 2>&1
+sudo sed -i -- 's/azureuser ALL=(ALL) ALL/azureuser ALL=(ALL) NOPASSWD:ALL/g' /etc/sudoers.d/waagent
 
 # Update master node
 echo $MASTER_IP $MASTER_NAME >> /etc/hosts
@@ -51,16 +54,16 @@ fi
 sudo yum -y install epel-release
 
 # Install sshpass to automate ssh-copy-id action
-sudo yum install -y sshpass >> /tmp/azuredeploy.log.$$ 2>&1
+sudo yum install -y sshpass
 
 # Install ansible and start the ansible hosts file
-sudo yum -y install ansible >> /tmp/azuredeploy.log.$$ 2>&1
+sudo yum -y install ansible
 sudo echo "[master]" > /etc/ansible/hosts
 sudo echo $MASTER_NAME >> /etc/ansible/hosts
 sudo echo "[workers]" >> /etc/ansible/hosts
 
 # Install software needed for NFS server
-sudo yum -y install nfs-utils libnfsidmap >> /tmp/azuredeploy.log.$$ 2>&1
+sudo yum -y install nfs-utils libnfsidmap
 sudo systemctl enable rpcbind
 sudo systemctl enable nfs-server
 sudo systemctl start rpcbind
@@ -71,10 +74,10 @@ sudo systemctl start nfs-idmapd
 # Configure data disks and export via an NFS share
 DATADISKS="$(lsblk -dlnpo name | grep -v -E 'sda|sdb|fd0|sr0')"
 
-sudo pvcreate $DATADISKS >> /tmp/azuredeploy.log.$$ 2>&1
-sudo vgcreate vg_data $DATADISKS >> /tmp/azuredeploy.log.$$ 2>&1
-sudo lvcreate -n lv_data -l 100%FREE vg_data >> /tmp/azuredeploy.log.$$ 2>&1
-sudo mkfs.xfs /dev/vg_data/lv_data >> /tmp/azuredeploy.log.$$ 2>&1
+sudo pvcreate $DATADISKS
+sudo vgcreate vg_data $DATADISKS
+sudo lvcreate -n lv_data -l 100%FREE vg_data
+sudo mkfs.xfs /dev/vg_data/lv_data
 sudo mkdir /data
 sudo echo -e "/dev/mapper/vg_data-lv_data\t/data\txfs\tdefaults\t0 0" >> /etc/fstab
 sudo mount /data
@@ -95,7 +98,7 @@ i=0
 while [ $i -lt $NUM_OF_VM ]
 do
    workerip=`expr $i + $WORKER_IP_START`
-   echo 'I update host - '$WORKER_NAME$i >> /tmp/azuredeploy.log.$$ 2>&1
+   echo 'I update host - '$WORKER_NAME$i
    echo $WORKER_IP_BASE$workerip $WORKER_NAME$i >> /etc/hosts
    echo $WORKER_IP_BASE$workerip $WORKER_NAME$i >> /tmp/hosts.$$
    sudo -u $ADMIN_USERNAME sh -c "sshpass -p '$ADMIN_PASSWORD' ssh-copy-id $WORKER_NAME$i"
@@ -110,12 +113,12 @@ sudo yum -y install rpm-build
 # Build SLURM on master node
 ###################################
 
-wget https://download.schedmd.com/slurm/slurm-17.11.8.tar.bz2 >> /tmp/azuredeploy.log.$$ 2>&1
-sudo yum install -y hwloc-devel hwloc-libs hdf5-devel munge munge-devel munge-libs numactl-devel numactl-libs readline-devel openssl-devel pam-devel perl-ExtUtils-MakeMaker mariadb-devel >> /tmp/azuredeploy.log.$$ 2>&1
-rpmbuild -ta slurm-17.11.8.tar.bz2 >> /tmp/azuredeploy.log.$$ 2>&1
+wget https://download.schedmd.com/slurm/slurm-17.11.8.tar.bz2
+sudo yum install -y hwloc-devel hwloc-libs hdf5-devel munge munge-devel munge-libs numactl-devel numactl-libs readline-devel openssl-devel pam-devel perl-ExtUtils-MakeMaker mariadb-devel
+rpmbuild -ta slurm-17.11.8.tar.bz2
 
 # Generate the munge key
-echo "Generating munge key" >> /tmp/azuredeploy.log.$$ 2>&1
+echo "Generating munge key"
 dd if=/dev/urandom bs=1 count=1024 >/tmp/munge.key
 sudo chown munge:munge /tmp/munge.key
 sudo chmod 600 /tmp/munge.key
@@ -132,54 +135,54 @@ sudo yum -y install /rpmbuild/RPMS/x86_64/slurm-17.11.8-1.el7.centos.x86_64.rpm 
 /rpmbuild/RPMS/x86_64/slurm-perlapi-17.11.8-1.el7.centos.x86_64.rpm \
 /rpmbuild/RPMS/x86_64/slurm-libpmi-17.11.8-1.el7.centos.x86_64.rpm \
 /rpmbuild/RPMS/x86_64/slurm-slurmctld-17.11.8-1.el7.centos.x86_64.rpm \
-/rpmbuild/RPMS/x86_64/slurm-slurmdbd-17.11.8-1.el7.centos.x86_64.rpm >> /tmp/azuredeploy.log.$$ 2>&1
+/rpmbuild/RPMS/x86_64/slurm-slurmdbd-17.11.8-1.el7.centos.x86_64.rpm
 
 # Download slurm.conf and fill in the node info
 SLURMCONF=/tmp/slurm.conf.$$
-wget $TEMPLATE_BASE/slurm.template.conf -O $SLURMCONF >> /tmp/azuredeploy.log.$$ 2>&1
-sed -i -- 's/__MASTERNODE__/'"$MASTER_NAME"'/g' $SLURMCONF >> /tmp/azuredeploy.log.$$ 2>&1
+wget $TEMPLATE_BASE/slurm.template.conf -O $SLURMCONF
+sed -i -- 's/__MASTERNODE__/'"$MASTER_NAME"'/g' $SLURMCONF
 lastvm=`expr $NUM_OF_VM - 1`
-sed -i -- 's/__WORKERNODES__/'"$WORKER_NAME"'[0-'"$lastvm"']/g' $SLURMCONF >> /tmp/azuredeploy.log.$$ 2>&1
-sudo cp -f $SLURMCONF /etc/slurm/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
-sudo chown slurm /etc/slurm/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
+sed -i -- 's/__WORKERNODES__/'"$WORKER_NAME"'[0-'"$lastvm"']/g' $SLURMCONF
+sudo cp -f $SLURMCONF /etc/slurm/slurm.conf
+sudo chown slurm /etc/slurm/slurm.conf
 sudo chmod o+w /var/spool # Write access for slurmctld log. Consider switch log file to another location
-sudo systemctl daemon-reload >> /tmp/azuredeploy.log.$$ 2>&1
-sudo systemctl enable munge >> /tmp/azuredeploy.log.$$ 2>&1
-sudo systemctl start munge >> /tmp/azuredeploy.log.$$ 2>&1 # Start munged
-sudo systemctl enable slurmctld >> /tmp/azuredeploy.log.$$ 2>&1
-sudo systemctl start  slurmctld >> /tmp/azuredeploy.log.$$ 2>&1 # Start the master daemon service
+sudo systemctl daemon-reload
+sudo systemctl enable munge
+sudo systemctl start munge # Start munged
+sudo systemctl enable slurmctld
+sudo systemctl start  slurmctld # Start the master daemon service
 
 # Set up the Ansible playbook that can build the /etc/slurm/slurm.conf file
 PLAYBOOK=/tmp/create_slurm_conf.yml.$$
-wget $TEMPLATE_BASE/create_slurm_conf.yml -O $PLAYBOOK >> /tmp/azuredeploy.log.$$ 2>&1
-sed -i -- 's/__MASTERNODE__/'"$MASTER_NAME"'/g' $PLAYBOOK >> /tmp/azuredeploy.log.$$ 2>&1
+wget $TEMPLATE_BASE/create_slurm_conf.yml -O $PLAYBOOK
+sed -i -- 's/__MASTERNODE__/'"$MASTER_NAME"'/g' $PLAYBOOK
 cp -f $PLAYBOOK /home/$ADMIN_USERNAME/create_slurm_conf.yml
 sudo chown $ADMIN_USERNAME /home/$ADMIN_USERNAME/create_slurm_conf.yml
 SLURMJ2=/tmp/slurm_conf.j2.$$
-wget $TEMPLATE_BASE/slurm_conf.j2 -O $SLURMJ2 >> /tmp/azuredeploy.log.$$ 2>&1
+wget $TEMPLATE_BASE/slurm_conf.j2 -O $SLURMJ2
 cp -f $SLURMJ2 /home/$ADMIN_USERNAME/slurm_conf.j2
 sudo chown $ADMIN_USERNAME /home/slurm_conf.j2
 
 # Download worker_config.sh and add admin password for sudo
 WORKERCONFIG=/tmp/worker_config.sh.$$
-wget $TEMPLATE_BASE/worker_config.sh -O $WORKERCONFIG >> /tmp/azuredeploy.log.$$ 2>&1
-sed -i -- 's/__ADMINPASS__/'"$ADMIN_PASSWORD"'/g' $WORKERCONFIG >> /tmp/azuredeploy.log.$$ 2>&1
+wget $TEMPLATE_BASE/worker_config.sh -O $WORKERCONFIG
+sed -i -- 's/__ADMINPASS__/'"$ADMIN_PASSWORD"'/g' $WORKERCONFIG
 
 # Download the scripts to create users
 READCSV=/tmp/user_read_csv_create_yml.py.$$
-wget $TEMPLATE_BASE/user_read_csv_create_yml.py -O $READCSV >> /tmp/azuredeploy.log.$$ 2>&1
+wget $TEMPLATE_BASE/user_read_csv_create_yml.py -O $READCSV
 cp -f $READCSV /home/$ADMIN_USERNAME/user_read_csv_create_yml.py
 sudo chown $ADMIN_USERNAME /home/$ADMIN_USERNAME/user_read_csv_create_yml.py
 CREATEUSERS=/tmp/create_users.yml.$$
-wget $TEMPLATE_BASE/create_users.yml -O $CREATEUSERS >> /tmp/azuredeploy.log.$$ 2>&1
+wget $TEMPLATE_BASE/create_users.yml -O $CREATEUSERS
 cp -f $CREATEUSERS /home/$ADMIN_USERNAME/create_users.yml
 sudo chown $ADMIN_USERNAME /home/$ADMIN_USERNAME/create_users.yml
 
 # Download the SSH configuration files
 SSHDCONFIG=/tmp/sshd_config.$$
-wget $TEMPLATE_BASE/sshd_config -O $SSHDCONFIG >> /tmp/azuredeploy.log.$$ 2>&1
+wget $TEMPLATE_BASE/sshd_config -O $SSHDCONFIG
 SSHCONFIG=/tmp/ssh_config.$$
-wget $TEMPLATE_BASE/ssh_config -O $SSHCONFIG >> /tmp/azuredeploy.log.$$ 2>&1
+wget $TEMPLATE_BASE/ssh_config -O $SSHCONFIG
 
 # Start building needed SSH files used for host authentication
 ssh_known_hosts=/tmp/ssh_known_hosts.$$
@@ -189,33 +192,33 @@ echo 'master' > $shosts_equiv
 
 # Install slurm on all nodes
 # Also push munge key and slurm.conf to them
-echo "Prepare the local copy of munge key" >> /tmp/azuredeploy.log.$$ 2>&1 
+echo "Prepare the local copy of munge key" 
 
 mungekey=/tmp/munge.key.$$
 sudo cp -f /etc/munge/munge.key $mungekey
 sudo chown $ADMIN_USERNAME $mungekey
 
-echo "Start looping all workers" >> /tmp/azuredeploy.log.$$ 2>&1 
+echo "Start looping all workers" 
 shosts.equiv
 i=0
 while [ $i -lt $NUM_OF_VM ]
 do
    worker=$WORKER_NAME$i
 
-   echo "SCP to $worker"  >> /tmp/azuredeploy.log.$$ 2>&1 
-   sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key >> /tmp/azuredeploy.log.$$ 2>&1 
-   sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
-   sudo -u $ADMIN_USERNAME scp $WORKERCONFIG $ADMIN_USERNAME@$worker:/tmp/worker_config.sh >> /tmp/azuredeploy.log.$$ 2>&1
-   sudo -u $ADMIN_USERNAME scp /tmp/hosts.$$ $ADMIN_USERNAME@$worker:/tmp/hosts >> /tmp/azuredeploy.log.$$ 2>&1
-   sudo -u $ADMIN_USERNAME scp /rpmbuild/RPMS/x86_64/slurm-17.11.8-1.el7.centos.x86_64.rpm $ADMIN_USERNAME@$worker:/tmp/slurm-17.11.8-1.el7.centos.x86_64.rpm >> /tmp/azuredeploy.log.$$ 2>&1
-   sudo -u $ADMIN_USERNAME scp /rpmbuild/RPMS/x86_64/slurm-contribs-17.11.8-1.el7.centos.x86_64.rpm $ADMIN_USERNAME@$worker:/tmp/slurm-contribs-17.11.8-1.el7.centos.x86_64.rpm >> /tmp/azuredeploy.log.$$ 2>&1
-   sudo -u $ADMIN_USERNAME scp /rpmbuild/RPMS/x86_64/slurm-example-configs-17.11.8-1.el7.centos.x86_64.rpm $ADMIN_USERNAME@$worker:/tmp/slurm-example-configs-17.11.8-1.el7.centos.x86_64.rpm >> /tmp/azuredeploy.log.$$ 2>&1
-   sudo -u $ADMIN_USERNAME scp /rpmbuild/RPMS/x86_64/slurm-libpmi-17.11.8-1.el7.centos.x86_64.rpm $ADMIN_USERNAME@$worker:/tmp/slurm-libpmi-17.11.8-1.el7.centos.x86_64.rpm >> /tmp/azuredeploy.log.$$ 2>&1
-   sudo -u $ADMIN_USERNAME scp /rpmbuild/RPMS/x86_64/slurm-pam_slurm-17.11.8-1.el7.centos.x86_64.rpm $ADMIN_USERNAME@$worker:/tmp/slurm-pam_slurm-17.11.8-1.el7.centos.x86_64.rpm >> /tmp/azuredeploy.log.$$ 2>&1
-   sudo -u $ADMIN_USERNAME scp /rpmbuild/RPMS/x86_64/slurm-slurmd-17.11.8-1.el7.centos.x86_64.rpm $ADMIN_USERNAME@$worker:/tmp/slurm-slurmd-17.11.8-1.el7.centos.x86_64.rpm >> /tmp/azuredeploy.log.$$ 2>&1
+   echo "SCP to $worker"  
+   sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key 
+   sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf
+   sudo -u $ADMIN_USERNAME scp $WORKERCONFIG $ADMIN_USERNAME@$worker:/tmp/worker_config.sh
+   sudo -u $ADMIN_USERNAME scp /tmp/hosts.$$ $ADMIN_USERNAME@$worker:/tmp/hosts
+   sudo -u $ADMIN_USERNAME scp /rpmbuild/RPMS/x86_64/slurm-17.11.8-1.el7.centos.x86_64.rpm $ADMIN_USERNAME@$worker:/tmp/slurm-17.11.8-1.el7.centos.x86_64.rpm
+   sudo -u $ADMIN_USERNAME scp /rpmbuild/RPMS/x86_64/slurm-contribs-17.11.8-1.el7.centos.x86_64.rpm $ADMIN_USERNAME@$worker:/tmp/slurm-contribs-17.11.8-1.el7.centos.x86_64.rpm
+   sudo -u $ADMIN_USERNAME scp /rpmbuild/RPMS/x86_64/slurm-example-configs-17.11.8-1.el7.centos.x86_64.rpm $ADMIN_USERNAME@$worker:/tmp/slurm-example-configs-17.11.8-1.el7.centos.x86_64.rpm
+   sudo -u $ADMIN_USERNAME scp /rpmbuild/RPMS/x86_64/slurm-libpmi-17.11.8-1.el7.centos.x86_64.rpm $ADMIN_USERNAME@$worker:/tmp/slurm-libpmi-17.11.8-1.el7.centos.x86_64.rpm
+   sudo -u $ADMIN_USERNAME scp /rpmbuild/RPMS/x86_64/slurm-pam_slurm-17.11.8-1.el7.centos.x86_64.rpm $ADMIN_USERNAME@$worker:/tmp/slurm-pam_slurm-17.11.8-1.el7.centos.x86_64.rpm
+   sudo -u $ADMIN_USERNAME scp /rpmbuild/RPMS/x86_64/slurm-slurmd-17.11.8-1.el7.centos.x86_64.rpm $ADMIN_USERNAME@$worker:/tmp/slurm-slurmd-17.11.8-1.el7.centos.x86_64.rpm
 
-   echo "Remote execute on $worker" >> /tmp/azuredeploy.log.$$ 2>&1 
-   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker 'sh /tmp/worker_config.sh' >> /tmp/azuredeploy.log.$$ 2>&1
+   echo "Remote execute on $worker" 
+   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker 'sh /tmp/worker_config.sh'
 
    # While we're looping through all the workers grab the ssh host keys for each to deploy ssh_known_hosts afterwards
    /usr/bin/ssh-keyscan $worker >> $ssh_known_hosts
@@ -236,17 +239,17 @@ while [ $i -lt $NUM_OF_VM ]
 do
    worker=$WORKER_NAME$i
 
-   echo "SCP to $worker"  >> /tmp/azuredeploy.log.$$ 2>&1 
-   sudo -u $ADMIN_USERNAME scp $ssh_known_hosts $ADMIN_USERNAME@$worker:/tmp/ssh_known_hosts >> /tmp/azuredeploy.log.$$ 2>&1 
-   sudo -u $ADMIN_USERNAME scp $shosts_equiv $ADMIN_USERNAME@$worker:/tmp/shosts.equiv >> /tmp/azuredeploy.log.$$ 2>&1 
-   sudo -u $ADMIN_USERNAME scp $SSHDCONFIG $ADMIN_USERNAME@$worker:/tmp/sshd_config >> /tmp/azuredeploy.log.$$ 2>&1 
-   sudo -u $ADMIN_USERNAME scp $SSHCONFIG $ADMIN_USERNAME@$worker:/tmp/ssh_config >> /tmp/azuredeploy.log.$$ 2>&1 
+   echo "SCP to $worker"  
+   sudo -u $ADMIN_USERNAME scp $ssh_known_hosts $ADMIN_USERNAME@$worker:/tmp/ssh_known_hosts 
+   sudo -u $ADMIN_USERNAME scp $shosts_equiv $ADMIN_USERNAME@$worker:/tmp/shosts.equiv 
+   sudo -u $ADMIN_USERNAME scp $SSHDCONFIG $ADMIN_USERNAME@$worker:/tmp/sshd_config 
+   sudo -u $ADMIN_USERNAME scp $SSHCONFIG $ADMIN_USERNAME@$worker:/tmp/ssh_config 
 
-   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker 'sudo cp /tmp/ssh_known_hosts /etc/ssh/ssh_known_hosts' >> /tmp/azuredeploy.log.$$ 2>&1
-   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker 'sudo cp /tmp/shosts.equiv /etc/ssh/shosts.equiv' >> /tmp/azuredeploy.log.$$ 2>&1
-   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker 'sudo cp /tmp/sshd_config /etc/ssh/sshd_config' >> /tmp/azuredeploy.log.$$ 2>&1
-   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker 'sudo cp /tmp/ssh_config /etc/ssh/ssh_config' >> /tmp/azuredeploy.log.$$ 2>&1
-   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker 'sudo systemctl restart sshd' >> /tmp/azuredeploy.log.$$ 2>&1
+   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker 'sudo cp /tmp/ssh_known_hosts /etc/ssh/ssh_known_hosts'
+   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker 'sudo cp /tmp/shosts.equiv /etc/ssh/shosts.equiv'
+   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker 'sudo cp /tmp/sshd_config /etc/ssh/sshd_config'
+   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker 'sudo cp /tmp/ssh_config /etc/ssh/ssh_config'
+   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker 'sudo systemctl restart sshd'
 
    i=`expr $i + 1`
 done
