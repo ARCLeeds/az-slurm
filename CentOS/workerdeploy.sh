adba5af39e603b4d588e7b41c365b8150538ffbb
John Hodrien <J.H.Hodrien@leeds.ac.uk>
2020-11-16 15:37:22 +0000

    Faulty path

--- CentOS/workerdeploy.sh
+++ CentOS/workerdeploy.sh
@@ -14,7 +14,7 @@ bash -c 'echo -e "/data/home /home none bind 0 0" >> /etc/fstab'
 mount -a
 setsebool -P use_nfs_home_dirs=on
 
-cp /data/ssh/ssh* /etc/ssh
+cp /data/system/ssh/ssh* /etc/ssh
 service sshd restart
 
 cp /data/system/hosts /etc/hosts
