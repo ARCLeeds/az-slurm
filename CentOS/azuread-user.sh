f594634139ec0de3c1f98249bd495aff4655c910
John Hodrien <J.H.Hodrien@leeds.ac.uk>
2020-11-19 20:43:24 +0000

    Only assign accounts to the master

--- CentOS/azuread-user.sh
+++ CentOS/azuread-user.sh
@@ -3,8 +3,8 @@
 # The currently authenticated user gets admin rights to all machines
 username=$(az account show --query user.name --output tsv)
 vm1=$(az vm show --resource-group $RG --name master --query id -o tsv)
-vm2=$(az vm show --resource-group $RG --name worker0 --query id -o tsv)
-vm3=$(az vm show --resource-group $RG --name worker1 --query id -o tsv)
+#vm2=$(az vm show --resource-group $RG --name worker0 --query id -o tsv)
+#vm3=$(az vm show --resource-group $RG --name worker1 --query id -o tsv)
 
 for vm in $vm1 $vm2 $vm3;do
   az role assignment create \
