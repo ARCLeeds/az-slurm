5a88c5406b9b509d6e35b2a22a1f449f46013e21
John Hodrien <J.H.Hodrien@leeds.ac.uk>
2020-11-20 12:55:05 +0000

    Switch to using groups for user/admin auth

--- CentOS/deploy.sh
+++ CentOS/deploy.sh
@@ -3,5 +3,3 @@
 . private.sh
 
 az deployment group create --name azure-slurm-test --resource-group $RG --template-file azuredeploy.json --parameters azuredeploy.parameters.json
-
-./azuread-user.sh
